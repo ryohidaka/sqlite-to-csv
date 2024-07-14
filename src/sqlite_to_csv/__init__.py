aad269996b42106f08b790dad65e0dd640b3426c
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-07-15 00:35:19 +0900

    feat: Add SqliteToCSV class.

--- src/sqlite_to_csv/__init__.py
+++ src/sqlite_to_csv/__init__.py
@@ -1,3 +1,15 @@
 def main() -> int:
     print("Hello from sqlite-to-csv!")
+    app = SqliteToCSV()
     return 0
+
+
+class SqliteToCSV:
+    """
+    Export data from a SQLite database to CSV files.
+    """
+
+    def __init__(
+        self,
+    ):
+        return
